7dcf4c10f51ca16d7e163917da2a024ba8b182b1
Renato <renato_oliveiraandrade@hotmail.com>
2022-10-24 20:50:15 -0300

    initial commit

---
+++ src/components/Counter.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+
+
+
+
+
+function Counter() {
+    const [count, setCount] = useState(0)
+
+        
+    
+    return ( 
+        <>
+        <h1>{count}</h1>
+        <button onClick={()=>setCount(count-1)}>-</button>
+        <button onClick={()=>setCount(count+1)}>+</button>
+
+
+
+
+        </>
+     );
+}
+
+export default Counter;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import App from '../App';
+import '../App.css'
+
+function Header () {
+    return (
+        <div className="head">
+        
+        <h1>Contador</h1>
+        <p>Developed by Renato</p>
+        
+        </div>
+      );
+}
+
+export default Header ;
